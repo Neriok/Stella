122f58b8ba1e1f70a8c63c59c7c98ad0004a6270
Leonardo Larrad <larradleonardo@gmail.com>
2018-04-24 23:28:14 -0400

    Add RemoveReference to test Stella::Move()

--- Source/Source.cpp
+++ Source/Source.cpp
@@ -9,11 +9,33 @@
 
 using namespace Stella;
 
+template<typename T>
+struct RemoveReference
+{
+	typedef T Type;
+};
+
+template<typename T>
+__forceinline typename RemoveReference<T>::Type&& Move(T&& obj)
+{
+	typedef typename RemoveReference<T>::Type CastType;
+	return (CastType&&)obj;
+}
+
+template<typename T>
+__forceinline T&& Move2(T&& obj)
+{
+	return obj;
+}
+
 int main()
 {
-	String k = "HOLA MUNDO!";
-	std::cout << k;
 
+	String n = "Hola mundo!";
+	
+	String k = String(Move2(n));
+
+	std::cout << k;
 	getchar();
 	return 0;
 }
